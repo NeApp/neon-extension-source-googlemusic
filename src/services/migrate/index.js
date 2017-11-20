1d842efed7142e7a0604e7b5633d845a57f4c2d9
Dean Gardiner <me@dgardiner.net>
2017-11-21 11:53:30 +1300

    Removed legacy preference migration from the migrate service

--- src/services/migrate/index.js
+++ src/services/migrate/index.js
@@ -1,21 +1,11 @@
-import Log from 'neon-extension-framework/core/logger';
 import MigrateService from 'neon-extension-framework/services/migrate';
 import Plugin from 'neon-extension-source-googlemusic/core/plugin';
-import Preferences from 'neon-extension-framework/preferences';
 import Registry from 'neon-extension-framework/core/registry';
 
 
 export class GoogleMusicMigrateService extends MigrateService {
     constructor() {
         super(Plugin);
-    }
-
-    onLegacyPreferences(preferences) {
-        Log.info('Migrating preferences...');
-
-        // Enable plugin
-        return Plugin.registerContentScripts()
-            .then(() => Preferences.putBoolean(Plugin.id + ':enabled', true));
     }
 }
 
