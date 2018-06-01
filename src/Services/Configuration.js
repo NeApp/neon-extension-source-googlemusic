b2ee9032780e3b06b6a1635a1843b4795edc9c16
Dean Gardiner <me@dgardiner.net>
2018-06-01 13:51:17 +1200

    Flattened services

--- src/Services/Configuration.js
+++ src/Services/Configuration.js
@@ -1,9 +1,20 @@
 import ConfigurationService from 'neon-extension-framework/Services/Configuration';
 import Plugin from 'neon-extension-source-googlemusic/Core/Plugin';
 import Registry from 'neon-extension-framework/Core/Registry';
+import {Page} from 'neon-extension-framework/Models/Configuration';
+import {EnableOption} from 'neon-extension-framework/Models/Configuration/Options'
 
-import Options from './options';
+export const Options = [
+    new Page(Plugin, null, [
+        new EnableOption(Plugin, 'enabled', {
+            default: false,
 
+            type: 'plugin',
+            permissions: true,
+            contentScripts: true
+        })
+    ])
+];
 
 export class GoogleMusicConfigurationService extends ConfigurationService {
     constructor() {
